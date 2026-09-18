c97c07d478ac5db7118b6e125be770425b975eae
Duc Anh Vo <QuangDucAnh.Vo@student.uts.edu.au>
2026-09-18 16:34:34 +1000

    Add music controller scripts

--- Assets/Scripts/BackgroundMusicPlayer.cs
+++ Assets/Scripts/BackgroundMusicPlayer.cs
@@ -3,19 +3,42 @@ using UnityEngine;
 public class BackgroundMusicPlayer : MonoBehaviour
 {
     public AudioClip introMusic;
-
     public AudioClip ghostNormalMusic;
+    private AudioSource audioSource;
     private float timer = 0.0f;     
-    private float switchTime;        
+    private float switchTime; 
+    private bool hasSwitched = false;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         
+        switchTime = Mathf.Min(3.0f, introMusic.length);
+        
+        audioSource.clip = introMusic;
+        audioSource.loop = false;
+        audioSource.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (hasSwitched)
+        {
+            return;
+        }
+        
+        timer = timer + Time.deltaTime;
+ 
+        if (timer >= switchTime)
+        {
+            hasSwitched = true;
+            
+            audioSource.clip = ghostNormalMusic;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
         
     }
 }
